5d86246cb685bfbab85770c5d6243ac99ee623fe
sankerdas.yes@gmail.com <sanker@ibosoninnov.com>
2021-03-21 23:17:44 +0530

    Creating a method & binding it to an event with v-on, Binding data to inputs with v-model
    Creating a method & binding it to an event with v-on
    
    Binding data to inputs with v-model
    
    bind data and input using v-model and logged  the value

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,9 +1,9 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmit" >
       <label for="new-todo-input" >
           What you want to ToDo?
       </label>
-      <input type="text" id="new-todo-input" name="new-todo" autocomplete="off">
+      <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model="label">
       <button type="submit" >
           Add
       </button>
@@ -12,7 +12,16 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            label: "",
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log("input value:- " + this.label);
+        }
+    }
 }
 </script>
 
